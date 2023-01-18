431d143fc41939bfd63e74415b64afee8c3c29b6
Serena Rosignoli <68196372+SerenaRosi@users.noreply.github.com>
2023-01-18 11:41:09 +0100

    Update csv2json.py

--- File-handling/csv2json.py
+++ File-handling/csv2json.py
@@ -1,10 +1,33 @@
 import csv
 import json
+import argparse
+import re
 
 final_dict = {}
 
-with open('DATABASE_CAS_new_all.csv', mode='r') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter='\t')
+parser = argparse.ArgumentParser(description="""
+
+From 'csv' to JSON
+
+    """, formatter_class= argparse.RawTextHelpFormatter)
+
+parser.add_argument('--input_file', '-i',
+                    type = str,
+                    required = True,
+                    help='''Name of the input file
+                     ''')
+
+
+args = parser.parse_args()
+
+# Get parameters
+if not re.search(".csv", args.input_file):
+    input_file = str(args.input_file + ".csv")
+else:
+    input_file = str(args.input_file)
+    
+with open(input_file, mode='r') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter='\t') ### CHANGE DELIMITER IF NECESSARY 
     line_count = 0
 
     for row in csv_reader:
@@ -26,6 +49,8 @@ with open('DATABASE_CAS_new_all.csv', mode='r') as csv_file:
 
 with open('database.json', 'w') as f:
     json.dump(final_dict, f, indent=2)
+
+
 
 
 
